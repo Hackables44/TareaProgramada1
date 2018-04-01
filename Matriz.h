2c8bb09e2c539f6664f851c20ee6d6104f5225bc
TheCcromer <33296759+TheCcromer@users.noreply.github.com>
2018-03-31 18:21:46 -0600

    update Matriz.h
    agregue todo lo necesario para el metodo crearMatrizFrecuencia

--- Matriz.h
+++ Matriz.h
@@ -10,8 +10,12 @@ class Matriz{
 		int columnas; // almacenará la cantidad de columnas de la matriz (define la otra dimensión)
     int n; //length de la matriz
 		int * * m; // almacenará la matriz; es puntero de punteros
-		int * * transpuesta;
-		int * * producto;
+		int * * transpuesta; // matriz transpuesta de la de frecuencia
+		int * * producto; // resultado del producto entre la matriz de frecuencia y su transpuesta
+		int * * frecuencia; // matriz de frecuencia
+		int count = 0; //contador para crear la matriz de frecuencia
+		int variable = 0;
+		int controlador = 1;
 
 	public: // especificador de acceso público
 	Matriz(int, int); // constructor que recibe la cantidad de filas y columnas
@@ -26,6 +30,7 @@ class Matriz{
 	int recursivo(int,int,int); //es el metodo recursivo que se encarga de calcular la frecuencia
 	void sacarTranspuesta(); //sacar la matriz transpuesta
 	void productoDeMatrices(); //multiplicacion de matriz normal por la transpuesta
+	void crearMatrizFrecuencia(); // crea la matriz de frecuencias
 }; // fin de la definición de la clase
 
 #endif // fin del prepocesador condicional
