67d4a1dfcfb36f3264dc949f8a0f319be715839f
Josh01B <37640012+Josh01B@users.noreply.github.com>
2018-03-31 20:57:31 -0600

    Legibilidad
    Tabulé los métodos

--- Matriz.h
+++ Matriz.h
@@ -8,7 +8,7 @@ class Matriz{
 		// variables de intancia o atributos
 		int filas; // almacena la cantidad de filas de la matriz (define una dimensión)
 		int columnas; // almacenará la cantidad de columnas de la matriz (define la otra dimensión)
-    int n; //length de la matriz
+    		int n; //length de la matriz
 		int * * m; // almacenará la matriz; es puntero de punteros
 		int * * transpuesta; // matriz transpuesta de la de frecuencia
 		int * * producto; // resultado del producto entre la matriz de frecuencia y su transpuesta
@@ -18,19 +18,19 @@ class Matriz{
 		int controlador = 1;
 
 	public: // especificador de acceso público
-	Matriz(int, int); // constructor que recibe la cantidad de filas y columnas
-	~Matriz(); // destructor del objeto Matriz
-	bool posValida(int , int); // verifica que la celda indicada tenga una posición válida
-	void setCelda(int, int, int); // set que recibe la posición de la celda y el número a asignar
-	int getCelda(int, int); // get recibe como parámetros la posición de la celda
-	int getFilas(); // devuelve la cantidad de filas de la matriz
-	int getColumnas(); // devuelve la cantidad de columnas de la matriz
-	void verMatriz(); // imprime en pantalla el contenido del arreglo;
-	int calcularFrecuencia(); //retorna la cantidad de frecuencia total al numero a analizar
-	int recursivo(int,int,int); //es el metodo recursivo que se encarga de calcular la frecuencia
-	void sacarTranspuesta(); //sacar la matriz transpuesta
-	void productoDeMatrices(); //multiplicacion de matriz normal por la transpuesta
-	void crearMatrizFrecuencia(); // crea la matriz de frecuencias
+		Matriz(int, int); // constructor que recibe la cantidad de filas y columnas
+		~Matriz(); // destructor del objeto Matriz
+		bool posValida(int , int); // verifica que la celda indicada tenga una posición válida
+		void setCelda(int, int, int); // set que recibe la posición de la celda y el número a asignar
+		int getCelda(int, int); // get recibe como parámetros la posición de la celda
+		int getFilas(); // devuelve la cantidad de filas de la matriz
+		int getColumnas(); // devuelve la cantidad de columnas de la matriz
+		void verMatriz(); // imprime en pantalla el contenido del arreglo;
+		int calcularFrecuencia(); //retorna la cantidad de frecuencia total al numero a analizar
+		int recursivo(int,int,int); //es el metodo recursivo que se encarga de calcular la frecuencia
+		void sacarTranspuesta(); //sacar la matriz transpuesta
+		void productoDeMatrices(); //multiplicacion de matriz normal por la transpuesta
+		void crearMatrizFrecuencia(); // crea la matriz de frecuencias
 }; // fin de la definición de la clase
 
 #endif // fin del prepocesador condicional
